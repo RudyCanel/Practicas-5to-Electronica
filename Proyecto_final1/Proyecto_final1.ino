b3f0f22422cdc85f66e7e33f1e0a892f9cf5851f
rcanel-2022449 <128450660+RudyCanel@users.noreply.github.com>
2023-05-25 10:58:50 -0600

    Update Proyecto_final1.ino

--- Proyecto_final1/Proyecto_final1.ino
+++ Proyecto_final1/Proyecto_final1.ino
@@ -1,3 +1,13 @@
+/* Fundacion Kinal
+* Centro educativo técnico laboral Kinal
+* Perito en electronica
+* Quinto perito
+* Sección: A
+* Curso : Taller de electronica digital y reparación de computadoras
+* Alumno: Rudy Santiago Canel Par
+* Carnet:2022449
+* Proyecto: Silo de granos
+*/
 // Librerias
 #include <Servo.h>
 #include <LiquidCrystal_I2C.h>
